190134ecb63a8fd46ecfdaee539964e8e7cfbb8d
ArtemKolesnyk <kolesnik2405@gmail.com>
2021-10-09 11:56:38 +0200

    task02

--- js/task-02.js
+++ js/task-02.js
@@ -6,4 +6,11 @@ const ingredients = [
     'Зелень',
     'Приправы',
   ];
-    
+  let ulList = document.querySelector("#ingredients");
+  const listIngredients = ingredients.forEach(ingredient => {
+  let newLi = document.createElement("li");
+  newLi.innerHTML = ingredient;
+  ulList.appendChild(newLi)});
+  
+
+console.log(ulList);
